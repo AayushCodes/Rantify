1b176a3e3d7a5688e9bc9f64b5f53b9b6a4b87d8
Aayush Gupta <aayushcodes@gmail.com>
2021-12-23 00:16:54 +0530

    pp added

---
+++ friday_god.py
@@ -0,0 +1,34 @@
+import yake
+from nltk.sentiment import SentimentIntensityAnalyzer
+kw_extractor = yake.KeywordExtractor()
+text = "I think I should not be doing engineering and open a small pastry shop in france. I will probably have a chance at love then and maybe not die a lonely virgin. I wish to live happily, bake by day and get baked by night. I would find love, be happy and be succesfull. Instead i choose to be miserable and commit suicide one day."
+sample = "I am so happy my heart is exploding. I want to fall in love. I have some hellish desires I wanna fulfill. The future appears to be so bright."
+lol = "i am sad"
+hep = 'i am happy'
+kk = 'I want to die'
+ll = 'I am the luckiest man in the world'
+longboi = 'I sometimes feel so empty inside that I talk to myself for hours at end. If I do not find anyone to love me soon, then I might just leap off a cliff and just die. I mean it gets so lonely sometimes. It\'s not even about being desperate or something but i feel so empty in my heart. There is no one to listen to me and no reason to live. I mean what am i doing wrong. I was once happy and now I just feel i have been dead inside for years.' 
+complex = 'I am kind of neutral today. I feel so hopeless sometimes but also so excited for some other things. Life has become an emotional rollecoster. I want to party but also remain in my bed all sad. I do not know what to anymore and it is the confusion that is eating me alive'
+friday = [text, lol, sample, hep, kk, ll, longboi, complex]
+language = "en"
+max_ngram_size = 3
+deduplication_threshold = 0.9
+numOfKeywords = 10
+custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+
+for i in friday:
+    keywords = custom_kw_extractor.extract_keywords(i)
+    pp = []
+    for kw in keywords:
+        pp.append(kw[0])
+    s = SentimentIntensityAnalyzer()
+    scores = s.polarity_scores(' '.join(pp))
+    print(i)
+    print('depression =', scores['neg'] + 0.000001)
+    print('heppy =', scores['pos'] + 0.000001)
+    heppy = scores['pos'] + 0.00000001
+    sed = scores['neg'] + 0.00000001
+    if heppy > sed:
+        print('heppy by - ', round(heppy/sed, 2))
+    else:
+        print('sed by -', round(sed/heppy, 2))
