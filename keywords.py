067f55b19da5d2df1b339a5e010182a6acbca61b
akashsinha-43 <akashsinha432003@gmail.com>
2021-12-22 23:07:56 +0530

    added sentiment analysis

--- keywords.py
+++ keywords.py
@@ -1,13 +1,16 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-friday_god = "I think I should not be doing engineering and open a small pastry shop in france. I will probably have a chance at love then and maybe not die a lonely virgin. I wish to live happily, bake by day and get baked by night. I would find love, be happy and be succesfull. Instead i choose to be miserable and commit suicide one day."
-friday_god = friday_god.lower()
+from nltk.sentiment import SentimentIntensityAnalyzer
+s = SentimentIntensityAnalyzer()
+sample = "I think I should not be doing engineering and open a small pastry shop in france. I will probably have a chance at love then and maybe not die a lonely virgin. I wish to live happily, bake by day and get baked by night. I would find love, be happy and be succesfull. Instead i choose to be miserable and commit suicide one day."
+sample = sample.lower()
 stop_words = set(stopwords.words('english'))
 stop_words.update(("i","would",",","."))
-word_tokens = word_tokenize(friday_god)
+word_tokens = word_tokenize(sample)
 filtered_sentence = []
 for w in word_tokens:
     if w not in stop_words:
         filtered_sentence.append(w)
 filtered_sentence = set(filtered_sentence)
 print((filtered_sentence))
+print(s.polarity_scores(sample))
